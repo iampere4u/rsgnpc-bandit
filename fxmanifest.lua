c35080c2652cbb0971c9824982c9a29bdf8f61e5
I AM PERE <75316668+iampere4u@users.noreply.github.com>
2024-07-01 12:06:16 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'adamant'
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-description 'NPC Bandits - Polísek'
-autor 'Polísek'
+description 'NPC Bandits - AMPFW'
+autor 'AMPFW'
 
 client_script {
 	'client/client.lua',
