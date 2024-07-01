9ed4b9d52f3f63625cfae3d556be663ca0579d5f
I AM PERE <75316668+iampere4u@users.noreply.github.com>
2024-07-01 16:28:37 +0530

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,6 +1,6 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
-RegisterServerEvent('pls_bandits:server:robbed')
-AddEventHandler('pls_bandits:server:robbed', function(amount,data)
+RegisterServerEvent('rsgnpc-bandits:server:robbed')
+AddEventHandler('rsgnpc-bandits:server:robbed', function(amount,data)
 	local src = source
 	local Player = RSGCore.Functions.GetPlayer(src)
 	Player.Functions.SetMoney('cash', 0)
